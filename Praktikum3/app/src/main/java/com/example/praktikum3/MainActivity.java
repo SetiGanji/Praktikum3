69166cfc66fe23d5aa034ad50d46161b350a3115
SetiGanji <44544383+SetiGanji@users.noreply.github.com>
2020-01-14 15:02:04 +0100

    Add files via upload

--- Praktikum3/app/src/main/java/com/example/praktikum3/MainActivity.java
+++ Praktikum3/app/src/main/java/com/example/praktikum3/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.praktikum3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editZeit;
+    EditText editDistanz;
+    EditText editMaxGeschw;
+
+    Switch swStillstand;
+    Switch swOnOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editZeit = findViewById(R.id.editZeit);
+        editDistanz = findViewById(R.id.editDistanz);
+        editMaxGeschw = findViewById(R.id.editMaxGeschw);
+        swStillstand = findViewById(R.id.swStillstand);
     }
 }
